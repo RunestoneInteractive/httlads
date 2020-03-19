751ec40ca60cb786a66f3e5f3b32cf327849da5e
Brad Miller <brad@runestoneinteractive.com>
2020-03-19 18:25:36 +0000

    Update for jupyter execute

--- conf.py
+++ conf.py
@@ -28,7 +28,7 @@ import pkg_resources
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.mathjax'] + runestone_extensions()
+extensions = ['sphinx.ext.mathjax', 'jupyter_sphinx.execute'] + runestone_extensions()
 
 #,'runestone.video','runestone.reveal','runestone.poll','runestone.tabbedStuff','runestone.disqus','runestone.codelens','runestone.activecode', 'runestone.assess', 'runestone.animation','runestone.meta', 'runestone.parsons', 'runestone.blockly', 'runestone.livecode','runestone.accessibility']
 
